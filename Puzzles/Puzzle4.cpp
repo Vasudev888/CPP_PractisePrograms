52ea230b66e9fdf2d4270e63adea27d400e454c8
vasudev8 <vdk5384@gmail.com>
2021-07-14 10:41:00 +0530

    puzzle 4 added

---
+++ Puzzles/Puzzle4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    a =10;
+    b = 4;
+    a = b;
+    b = 7;
+    cout  << "a : " << a;
+    cout << "b : " <<b;
+
+    return 0; 
+
+}
