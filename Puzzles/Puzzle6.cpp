dbc59a9296ed0d5dc08d0db3ed8e21b07fdc7c70
vasudev8 <vasudev@hanuvel.com>
2021-07-16 16:56:57 +0530

    Puzzle6

---
+++ Puzzles/Puzzle6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 20;
+    int b = 10;
+    int c = 15;
+    int d = 5;
+    int e;
+    e = a + b * c / d;
+    cout << e << endl;
+
+    return 0;
+}
