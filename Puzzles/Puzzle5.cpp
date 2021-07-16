aa6f00262a2341338f4f30cb3f7d7032b85ad5b7
vasudev8 <vasudev@hanuvel.com>
2021-07-16 16:45:57 +0530

    puzzle5

---
+++ Puzzles/Puzzle5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 0;
+    int b = 10;
+    if(a && b){
+        cout << "True" << endl;
+    }
+    else{
+        cout << "False" << endl;
+    }
+    return 0;
+}
